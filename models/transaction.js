f6237bbc4b55cbbd4f8bfc0058472df947a2a304
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2022-04-29 12:14:04 +0300

    declare models

---
+++ models/transaction.js
@@ -0,0 +1,45 @@
+const { DateTime } = require('luxon');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const consts = {
+    income: 'i',
+    outcome: 'o',
+}
+
+let TransactionSchema = new Schema(
+    {
+        type: {
+            type: String,
+            enum: [consts.income, consts.outcome],
+            required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        from: {
+            type: Schema.Types.ObjectId,
+            ref: 'Wallet',
+            required: true,
+        },
+        to: {
+            type: Schema.Types.ObjectId,
+            ref: 'Wallet',
+            required: true,
+        },
+        datetime: {
+            type: DateTime,
+            default: DateTime.now,
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: 'Category',
+            required: false,
+            default: undefined,
+        }
+    }
+)
+
+//Export model
+module.exports = mongoose.model('Transaction', TransactionSchema);
---
+++ models/wallet.js
@@ -0,0 +1,30 @@
+const { DateTime } = require("luxon");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+let WalletSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            maxlength: 20,
+        },
+        balance: {
+            type: Number,
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: ['Card', 'Cash'],
+            required: true,
+        },
+        created: {
+            type: DateTime,
+            default: DateTime.now,
+        }
+    }
+);
+
+//Export model
+module.exports = mongoose.model('Wallet', WalletSchema);
